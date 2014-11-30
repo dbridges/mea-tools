3dca9222908cdd0d03567f768cf0216ce4e36266
Daniel Bridges <bridges2@gmail.com>
2014-11-30 14:55:09 -0800

    moved to setuptools

--- mea-runner.py
+++ mea-runner.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import pymea.analog_display
+import pymea.interactive
 
 if __name__ == '__main__':
     if sys.argv[1] == 'view':
-        pymea.analog_display.run(sys.argv[2])
+        pymea.interactive.run(sys.argv[2])
