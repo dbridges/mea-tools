9fad9e1428b202fa1f3ce100016b2c4b9ba6c538
Daniel Bridges <bridges2@gmail.com>
2014-12-17 15:38:43 -0800

    Add export spikes subcommand.

--- mea-runner.py
+++ mea-runner.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+
+import pymea as mea
 import pymea.interactive
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'view':
+    if len(sys.argv) < 2:
+        print('No option specified.')
+        sys.exit()
+    elif sys.argv[1] == 'view':
         pymea.interactive.run(sys.argv[2])
+    elif sys.argv[1] == 'export_spikes':
+        files = [f for f in os.listdir() if f.endswith('h5')]
+        for i, f in enumerate(files):
+            mea.export_peaks(f)
+            print('%d of %d exported.' % (i, len(files)))
